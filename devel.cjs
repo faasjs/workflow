8f7b6a35f947be78b3c0126726e6ec5106c3c7b7
Ben <ben@zfben.com>
2022-09-05 07:22:58 +0000

    add devel version

---
+++ devel.cjs
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { version } = require('./package.json')
+const { execSync } = require('child_process')
+const { readFileSync, writeFileSync } = require('fs')
+
+const newVersion = version + '-devel-' + Date.now()
+
+for (const name of [
+  'react',
+  'step',
+  'types',
+]) {
+  const packagePath = __dirname + '/packages/' + name + '/package.json'
+  const pkg = JSON.parse(readFileSync(packagePath).toString())
+  pkg.version = newVersion
+  writeFileSync(packagePath, JSON.stringify(pkg, null, 2))
+  execSync('npm publish -w packages/' + name + ' --access public', { stdio: 'inherit' })
+  execSync(`npm dist-tag add ${pkg.name}@${newVersion} devel`)
+}
