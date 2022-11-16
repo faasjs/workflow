44c5ba65c8acf96acdb259f5b20982e83d4d72a5
Ben <ben@zfben.com>
2022-11-16 11:55:41 +0000

    update ci

--- jest.setup.ts
+++ jest.setup.ts
@@ -21,29 +21,58 @@ if (typeof window !== 'undefined') {
     })),
   })
 } else {
-  let knex: any
+  let init = false
 
   global.beforeAll(async () => {
+    if (init) return
+
+    if (!process.env.KNEX_CONNECTION_BASE)
+      process.env.KNEX_CONNECTION_BASE = 'postgresql://testing@pg_testing'
+
+    const db = `testing_${process.env.JEST_WORKER_ID}`
+
     if (!process.env.SECRET_KNEX_CONNECTION)
-      process.env.SECRET_KNEX_CONNECTION = `postgresql://testing@pg_testing${process.env.JEST_WORKER_ID}/testing`
+      process.env.SECRET_KNEX_CONNECTION = process.env.KNEX_CONNECTION_BASE + '/' + db
 
-    await useKnex({ config: { client: 'pg' } }).mount({ config: {} })
+    const base = Knex({
+      client: 'pg',
+      connection: process.env.KNEX_CONNECTION_BASE,
+    })
 
-    knex = Knex({
+    try {
+      await base.raw(`DROP DATABASE IF EXISTS ${db};`)
+    } catch (error) {
+      await base.raw(`SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '${db}' AND pid <> pg_backend_pid();`)
+    }
+
+    await base.raw(`CREATE DATABASE ${db};`)
+    await base.destroy()
+
+    const sub = Knex({
       client: 'pg',
       connection: process.env.SECRET_KNEX_CONNECTION,
     })
-    await knex.raw('DROP SCHEMA IF EXISTS public CASCADE;CREATE SCHEMA public;')
-    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";CREATE EXTENSION IF NOT EXISTS pg_trgm;')
-    await up(knex)
+
+    await sub.raw(`
+DROP SCHEMA IF EXISTS public CASCADE;CREATE SCHEMA public;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+`)
+
+    await up(sub)
+
+    await sub.destroy()
+
+    await useKnex({ config: { client: 'pg' } }).mount({ config: {} })
+
+    init = true
   })
 
   global.afterAll(async () => {
-    await knex.destroy()
     await useKnex().quit()
   })
 
   global.beforeEach(async () => {
-    await knex.raw('TRUNCATE steps RESTART IDENTITY;TRUNCATE step_records RESTART IDENTITY;')
+    await useKnex().raw('TRUNCATE steps RESTART IDENTITY;TRUNCATE step_records RESTART IDENTITY;')
   })
 }
